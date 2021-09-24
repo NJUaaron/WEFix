47bb1e7d46129d0ac462d3c3a5a54c053b80d39c
NJUaaron <aaronxyliu@qq.com>
2021-09-24 00:39:38 -0400

    regular update

--- test/hello.anal.js
+++ test/hello.anal.js
@@ -1,16 +1,32 @@
 const a =2;
 const {Builder, By, Key} = require('selenium-webdriver');
-async function main(){
-    const driver = await new Builder()
+const FTFixer = require('../index');
+async function main(){    const driver = await new Builder()
          .forBrowser('firefox')
-         .build();   // Initialize WebDriver
-    await driver.get('http://localhost:5000');
-    await driver.findElement(By.id('name')).sendKeys('Aaron');
-    await driver.findElement(By.id('email')).sendKeys('aaron@gmail.com', Key.ENTER);
+         .build()
+    await driver.executeScript(FTFixer.START_MO_SNIPPET);
+    var mutationsList = [];
+   // Initialize WebDriver
+    await driver.get('http://localhost:5000')
+
+    await driver.findElement(By.id('name')).sendKeys('Aaron')
+    await driver.manage().deleteAllCookies();
+
+    await driver.findElement(By.id('email')).sendKeys('aaron@gmail.com', Key.ENTER)
+    await FTFixer.waitFor(2000);
+    var cookies = await driver.manage().getCookies();
+    var mutations = FTFixer.parseCookie(cookies);
+    mutationsList.push(mutations);
+
     
-    var mListElement = await driver.findElement(By.id('mList'));
-    var mList = await mListElement.findElements(By.className('mInfo'));
-    expect(mList.length).toBe(3);
-    driver.close();
+    var mListElement = await driver.findElement(By.id('mList'))
+
+    var mList = await mListElement.findElements(By.className('mInfo'))
+
+    driver.close()
+
+
+    console.log(JSON.stringify(mutationsList));
+    return;
 }
 main();
