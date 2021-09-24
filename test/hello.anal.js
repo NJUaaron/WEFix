77495e29e45a8f3ecd3a5b38b5f5f44c5f397411
NJUaaron <aaronxyliu@qq.com>
2021-09-24 15:59:52 -0400

    regular update

--- test/hello.anal.js
+++ test/hello.anal.js
@@ -1,15 +1,19 @@
 const a =2;
 const {Builder, By, Key} = require('selenium-webdriver');
 const FTFixer = require('../index');
-async function main(){    const driver = await new Builder()
+async function main(){    var mutationsList = [];
+
+    const driver = await new Builder()
          .forBrowser('firefox')
          .build()
     await driver.executeScript(FTFixer.START_MO_SNIPPET);
-    var mutationsList = [];
+    mutationsList.push([]);
    // Initialize WebDriver
     await driver.get('http://localhost:5000')
+    mutationsList.push([]);
 
     await driver.findElement(By.id('name')).sendKeys('Aaron')
+    mutationsList.push([]);
     await driver.manage().deleteAllCookies();
 
     await driver.findElement(By.id('email')).sendKeys('aaron@gmail.com', Key.ENTER)
@@ -20,12 +24,17 @@ async function main(){    const driver = await new Builder()
 
     
     var mListElement = await driver.findElement(By.id('mList'))
+    mutationsList.push([]);
 
     var mList = await mListElement.findElements(By.className('mInfo'))
+    mutationsList.push([]);
+    mutationsList.push([]);
 
     driver.close()
+    mutationsList.push([]);
 
 
+    mutationsList.push([]);
     console.log(JSON.stringify(mutationsList));
     return;
 }
